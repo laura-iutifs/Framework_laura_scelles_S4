6171ac17f23a3fe88515ea741d44476af1720e8c
Laura <21701944@etu.unicaen.fr>
2019-03-24 16:04:06 +0100

    ajout menu pasge index + resolution erreur 405 je sais pas comment car apres voir charge la page de tres nombreuses fois ca c'est affiche --> ??

--- workspace/TP-SCELLES-2-1/src/main/java/s4/spring/controllers/loginController.java
+++ workspace/TP-SCELLES-2-1/src/main/java/s4/spring/controllers/loginController.java
@@ -40,49 +40,54 @@ public class loginController {
 		usersRepo.save(user);
 		return user.getIdentity() + " a ete ajoute a la base de donnee";
 	}
-	
+
 	@GetMapping("login")
-	public String Vuelogin(Model model) {
+	public String connexion(Model model) {
 		model.addAttribute("utilisateur", new User());
 		return "login";
 	}
-	
+
 	@PostMapping("login")
-	public RedirectView creerUser(Model model, User utilisateur, HttpSession session) {
+	public RedirectView connexion(Model model, User utilisateur, HttpSession session) {
 		// String login, String password, String email, String identity
 		User user = usersRepo.findByLogin(utilisateur.getLogin());
 
 		if (user != null) {
-			session.setAttribute("UtilisateurConnected", user);
+			System.err.println("user existe");
+			session.setAttribute("UtilisateurConnecte", user);
 			return new RedirectView("/index");
 		}
-		// si il n'existe pas alors on demande relogin
-		return new RedirectView("/login");
-	}
-
-	@GetMapping("delogin")
-	public RedirectView delogin(Model model, HttpSession session) {
-		session.removeAttribute("UtilisateurConnected");
+		System.out.println("user existe pas !");
+		// si il n'existe pas alors on demande reconnexion
 		return new RedirectView("/login");
 	}
 
 	@GetMapping("index")
 	public String index(Model model, HttpSession session) {
 		if (isConnected(session)) {
-			model.addAttribute("user", session.getAttribute("connectedUser"));
+			//on ajoute au model l utilisateur connecte et ses scripts
+			model.addAttribute("user", session.getAttribute("UtilisateurConnecte"));
 			model.addAttribute("scripts", scriptsRepo.findAll());
 			return "index";
-		}else {
-			return "index";
+		} else {
+			return "login";
 		}
 	}
-	
+
 	// si utilisateur connecte => true
 	public boolean isConnected(HttpSession session) {
-		if (session.getAttribute("connectedUser") != null) {
+		if (session.getAttribute("UtilisateurConnecte") != null) {
 			return true;
 		} else {
 			return false;
 		}
+	}
+	
+	// ------------------------------------
+	// revenir sur la page connexion si utilisateur se deconnecte
+	@GetMapping("deconnexion")
+	public RedirectView deconnexion(Model model, HttpSession session) {
+		session.removeAttribute("UtilisateurConnecte");
+		return new RedirectView("/login");
 	}
 }
