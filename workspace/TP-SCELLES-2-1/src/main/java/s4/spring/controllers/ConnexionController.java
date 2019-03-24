949cf45a31e9f2d007c01551d093666a846d969b
Laura <21701944@etu.unicaen.fr>
2019-03-24 14:43:03 +0100

    erreur 405 connexion ==> recherche

--- workspace/TP-SCELLES-2-1/src/main/java/s4/spring/controllers/ConnexionController.java
+++ workspace/TP-SCELLES-2-1/src/main/java/s4/spring/controllers/ConnexionController.java
@@ -2,11 +2,12 @@ package s4.spring.controllers;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -20,41 +21,67 @@ public class ConnexionController {
 
 	@Autowired
 	private UsersRepo usersRepo;
-	
+
 	@Autowired
 	private ScriptRepo scriptsRepo;
+
+	// ---------------------------------------
+
+	@RequestMapping("new")
+	@ResponseBody
+	public String newUtilisateur() {
+		User user = new User();
+		user.setLogin("laura");
+		user.setPassword("toor");
+		user.setIdentity("Laura SCELLES");
+		user.setEmail("laurascelles18@gmail.com");
+
+		usersRepo.save(user);
+		return user.getIdentity() + " a ete ajoute a la base de donnee";
+	}
 
 	@PostMapping("login")
 	public RedirectView creerUser(Model model, User utilisateur, HttpSession session) {
 		// String login, String password, String email, String identity
 		User user = usersRepo.findByLogin(utilisateur.getLogin());
-		
+
 		if (user != null) {
 			session.setAttribute("UtilisateurConnected", user);
 			return new RedirectView("/index");
 		}
-		//si il n'existe pas alors on demande reconnexion
+		// si il n'existe pas alors on demande reconnexion
 		return new RedirectView("/connexion");
 	}
-	
+
 	@GetMapping("connexion")
-	public String loginView(Model model) {
+	public String VueConnexion(Model model) {
 		model.addAttribute("utilisateur", new User());
 		return "connexion";
 	}
-	
+
 	@GetMapping("deconnexion")
 	public RedirectView deconnexion(Model model, HttpSession session) {
 		session.removeAttribute("UtilisateurConnected");
 		return new RedirectView("/connexion");
 	}
-	
+
 	@GetMapping("index")
 	public String index(Model model, HttpSession session) {
-		model.addAttribute("user", session.getAttribute("connectedUser"));
-		model.addAttribute("scripts", scriptsRepo.findAll());
-
-		return "index";
+		if (isConnected(session)) {
+			model.addAttribute("user", session.getAttribute("connectedUser"));
+			model.addAttribute("scripts", scriptsRepo.findAll());
+			return "index";
+		}else {
+			return "index";
+		}
 	}
 	
+	// si utilisateur connecte => true
+	public boolean isConnected(HttpSession session) {
+		if (session.getAttribute("connectedUser") != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
